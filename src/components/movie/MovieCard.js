1d65fafc8c2f990be8964ca26c55f73577e760bb
rikyhidayat21 <rikyhidayat21@gmail.com>
2021-10-22 20:58:55 +0700

    success create list movies

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn } from 'mdb-react-ui-kit';
 
-export default function MovieCard() {
+
+export default function MovieCard({ movie }) {
   return (
-    <div>
-      Movie Card
-    </div>
+    <>
+      <MDBCard style={{ maxWidth: '22rem' }}>
+        <MDBCardImage src={movie.Poster} position='top' alt='...' />
+        <MDBCardBody>
+          <MDBCardTitle>{movie.Title}</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </MDBCardText>
+          <MDBBtn href='#'>Button</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </>
   )
 }
--- src/components/movie/MovieList.js
+++ src/components/movie/MovieList.js
@@ -1,13 +1,43 @@
 import React from 'react'
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit'
 import { useSelector } from 'react-redux'
 import { getMovies } from '../../features/movie/movieSlice'
+import MovieCard from './MovieCard'
 
 export default function MovieList() {
   const movies = useSelector(getMovies)
-  console.log(movies, '<=== movies')
+  // console.log(movies, '<=== movies')
+
+  // check movies
+  // let checkMovies = ""
+  // checkMovies = movies.Response === "True" ? (
+  //   movies.Search.map((movie, idx) => (
+  //     <>
+  //       <MDBRow>
+  //         <MDBCol size='md' className='col-example'>
+  //           <MovieCard key={idx} movie={movie} />
+  //         </MDBCol>
+  //       </MDBRow>
+  //     </>
+  //   ))
+  // ) : (
+  //   <div>
+  //     {movies.Error}
+  //   </div>
+  // )
   return (
-    <div>
-      Movie list
-    </div>
+    <>
+      <div>
+
+        <MDBRow>
+          {movies && movies.Search.map((movie, idx) => (
+            <MDBCol key={idx} size='md' md="4" className='col-example mb-4'>
+              <MovieCard movie={movie} />
+            </MDBCol>
+          ))}
+        </MDBRow>
+
+      </div>
+    </>
   )
 }
